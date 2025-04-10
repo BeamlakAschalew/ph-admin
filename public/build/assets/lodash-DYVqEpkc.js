99050fd94d7bf19505c57afd8c1a9e3044b1a78e
Beamlak Aschalew <birrletej12@gmail.com>
2025-04-10 21:52:10 +0300

    added build files

--- public/build/assets/lodash-DYVqEpkc.js
+++ public/build/assets/lodash-DYVqEpkc.js
@@ -1,4 +1,4 @@
-import{x as Jr}from"./app-C2nkKKrM.js";var kt={exports:{}};/**
+import{B as Jr}from"./app-BBOiTIV0.js";var kt={exports:{}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
