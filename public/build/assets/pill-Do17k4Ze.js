2e29dc2b060fe89c0e113b4aec0480391a094331
Beamlak Aschalew <birrletej12@gmail.com>
2025-04-19 13:40:30 +0300

    added build files

--- public/build/assets/pill-Do17k4Ze.js
+++ public/build/assets/pill-Do17k4Ze.js
@@ -1,4 +1,4 @@
-import{D as c}from"./app-D4fyH1Zm.js";/**
+import{D as c}from"./app-BFqF2d0N.js";/**
  * @license lucide-vue-next v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- public/build/assets/x-DWBJJ3PO.js
+++ public/build/assets/x-DWBJJ3PO.js
@@ -1,4 +1,4 @@
-import{c as e}from"./pill-4H7IbqTR.js";/**
+import{c as e}from"./pill-Do17k4Ze.js";/**
  * @license lucide-vue-next v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
