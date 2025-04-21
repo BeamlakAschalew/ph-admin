2b9e46b0dbd07f2af08114baf5217469c360b122
Beamlak Aschalew <birrletej12@gmail.com>
2025-04-21 19:49:26 +0300

    fix: update sender email address in contact form feedback

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -32,7 +32,7 @@ class ContactController extends Controller
             // Update the email body to use a formatted HTML template
             Mail::send([], [], function ($message) use ($data) {
                 $message->to('birrletej12@gmail.com')
-                    ->from('appform@ph.beamlak.dev', 'App Feedback Form')
+                    ->from('ph1holders@gmail.com', 'Website Feedback Form')
                     ->subject('User Feedback')
                     ->setBody(new TextPart(
                         '<html><body>'.
