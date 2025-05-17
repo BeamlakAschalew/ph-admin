1672cc62b87c5962c368c83cfbc0d3390f200d9a
Beamlak Aschalew <birrletej12@gmail.com>
2025-05-17 15:02:02 +0300

    added build files

--- public_html/build/assets/x-buoaqqaZ.js
+++ public_html/build/assets/x-buoaqqaZ.js
@@ -1,4 +1,4 @@
-import{D as a}from"./app-DaqgdoDN.js";/**
+import{D as a}from"./app-rf_tLxYn.js";/**
  * @license lucide-vue-next v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
